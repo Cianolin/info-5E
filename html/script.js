ad6f51ab17ff44841bb45a25b89281f18c6e94f0
Cianolin <luciano.lin@iisviolamarchesini.edu.it>
2024-09-26 10:53:32 +0200

    continuo sito web

--- html/script.js
+++ html/script.js
@@ -1,26 +1,51 @@
 const catalogo = document.querySelectorAll('.catalog');
 const carrello = document.querySelectorAll('.cart');
-const container2= document.querySelector('.container2');
-const button= document.querySelector('button');
-const table= document.querySelector('#table');
-const listCart= document.querySelector('#listCart');
-const cell= document.querySelectorAll('td');
-let buttonCart=0;
-button.addEventListener('click', function(){
-    buttonCart++;
-    buttonCart=buttonCart%2;
-    console.log(buttonCart);
-    if(buttonCart==1){
-        container2.style.gridTemplateColumns='100% 0%';
-    } else {
-        container2.style.gridTemplateColumns='70% 30%';
-    }
+const container2 = document.querySelector('.container2');
+const button = document.querySelector('button');
+const table = document.querySelector('#table');
+const listCart = document.querySelector('#cart-items');
+const listItems = document.querySelectorAll('li');
+const listTd = document.querySelectorAll('td');
+const search = document.querySelector('#search');
+let selectedTd = null;
+let buttonCart = 0;
+button.addEventListener('click', function () {
+  buttonCart++;
+  buttonCart = buttonCart % 2;
+  console.log(buttonCart);
+  if (buttonCart == 1) {
+    container2.style.gridTemplateColumns = '100% 0%';
+  } else {
+    container2.style.gridTemplateColumns = '70% 30%';
+  }
 })
-  const listItems = document.querySelectorAll('li');
-
-  listItems.forEach(function(listItem) {
-    listItem.onmouseover = function() {
-      console.log('Ciao');
-    };
+listItems.forEach(function (listItem) {
+  listItem.onmouseover = function () {
+    console.log('Ciao');
+  };
+});
+listTd.forEach((td) => { td.setAttribute('draggable', 'true'); })
+search.addEventListener('focus', () => {
+  search.placeholder = '';
+})
+search.addEventListener('blur', () => {
+  search.placeholder = 'Search';
+})
+listTd.forEach(td => {
+  td.addEventListener('click', () => {
+    selectedTd = td;
   });
-
+});
+document.addEventListener('click', (event) => {
+  // Check if the clicked element is the currently selected td element
+  if (event.target === selectedTd) {
+    // Code to be executed when the selected td is clicked
+    console.log('Selected TD clicked!');
+    const li = document.createElement('li')
+  listCart.appendChild(li);
+  }
+});
+/*td => {td.addEventListener('click', liCart(td.textContent));}
+function liCart(text) {
+  
+}*/
