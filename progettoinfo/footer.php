02e6c33827f42902306d846a4a715cfb8acf8f91
Luciano Lin <luciano.lin@iisviolamarchesini.edu.it>
2025-01-30 22:44:11 +0100

    fine es

--- progettoinfo/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="bg-success text-center text-lg mt-auto">
-    <footer>
-        <p>Luciano Lin 5E 2024/25</p>
-    </footer>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
